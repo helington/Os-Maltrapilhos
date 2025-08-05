b4a67531cf080bfb6a9ddbbe65cf38315da34d48
Helington Willamy <willamy@Willamys-Mac.local>
2025-08-05 13:54:28 -0300

    chore: add path constants to the project

--- settings.py
+++ settings.py
@@ -1,5 +1,8 @@
+from os import path
+from pathlib import Path
+
 # Game settings
-SCREEN_HEIGHT = 800
+SCREEN_HEIGHT = 200
 SCREEN_WIDTH = 800
 GAME_NAME = "Os Maltrapilhos"
 FPS = 60
@@ -7,3 +10,10 @@ FPS = 60
 # Colors
 
 # Paths
+ROOT_PATH = Path(__file__).resolve().parents[0]
+ASSETS_PATH = path.join(ROOT_PATH, "assets")
+FONTS_PATH = path.join(ASSETS_PATH, "fonts")
+GRAPHICS_PATH = path.join(ASSETS_PATH, "graphics")
+BACKGROUND_PATH = path.join(GRAPHICS_PATH, "background")
+GROUND_PATH = path.join(GRAPHICS_PATH, "ground")
+SOUNDS_PATH = path.join(ASSETS_PATH, "sounds")
