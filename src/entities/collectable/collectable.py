12976f23567ac1b72f40562ccb64639a1ad7a967
Aldus Daniel (AldusD) <aldusdanielfsm@gmail.com>
2025-08-11 14:19:12 -0300

    fix: creates implementation for dev enviroment issue

--- src/entities/collectable/collectable.py
+++ src/entities/collectable/collectable.py
@@ -3,12 +3,14 @@ import pygame
 from os import path
 from ...config.paths import COLLECTABLES_PATH
 from .collectable_props import Collectable_Props
+from ..entities_enum import Item_code
 
 class Collectable(pygame.sprite.Sprite):
     def __init__(self, props: Collectable_Props):
         super().__init__()
         self.__dict__.update(props.__dict__)
         
+        self.load_image()
         image_path = path.join(COLLECTABLES_PATH, self.image)
         image = pygame.image.load(image_path).convert_alpha()
         scale = (64, 64) if self.image != "coin.png" else (40, 40)
@@ -16,6 +18,12 @@ class Collectable(pygame.sprite.Sprite):
         self.image = image
         self.rect = self.image.get_rect(center=(self.x, self.y))
         self.direction_handler = 0
+
+    def load_image(self):
+        if self.code == Item_code.COIN_CODE: self.image = "coin.png"
+        if self.code == Item_code.BUBBLE_CODE: self.image = "bubble.png"
+        if self.code == Item_code.MINIGUN_CODE: self.image = "minigun.png"
+        if self.code == Item_code.RIFLE_CODE: self.image = "rifle.png"
 
     def move(self):
         oscilation_slowness = 100
